32c81b55f6cf7639f6b00d6151828d168eda6aab
Rafał Świerczek <rafal.swierczek85@gmail.com>
2018-08-29 08:48:32 +0200

    Add Spring Data JPA Repositories. Closes #39

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/PetRepositories.java
@@ -0,0 +1,8 @@
+package guru.springframework.sfgpetclinic.repositories;
+
+
+import guru.springframework.sfgpetclinic.model.Pet;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PetRepositories extends CrudRepository<Pet, Long> {
+}
---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/PetTypeRepositry.java
@@ -0,0 +1,7 @@
+package guru.springframework.sfgpetclinic.repositories;
+
+import guru.springframework.sfgpetclinic.model.PetType;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PetTypeRepositry extends CrudRepository<PetType, Long>{
+}
